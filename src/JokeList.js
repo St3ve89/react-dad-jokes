28019618b4b0e79c716676c39bad6aee6871314b
Istvan Acs <istvan.acs89@gmail.com>
2019-06-16 16:28:15 +0100

    Synching with local storage

--- src/JokeList.js
+++ src/JokeList.js
@@ -13,18 +13,28 @@ class JokeList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      jokes: []
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
     }
   }
-  async componentDidMount() {
+
+  componentDidMount() {
     // Load Jokes
+    if(this.state.jokes.length === 0) this.getJokes()
+    
+  };
+
+  async getJokes() {
     let jokes = []
     while(jokes.length < this.props.numJokesToGet) {
       let res = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept: 'application/json'}});
       jokes.push({id: uuid(),text: res.data.joke, votes: 0 })
     }
     this.setState({jokes: jokes})
-  };
+    window.localStorage.setItem(
+      'jokes',
+      JSON.stringify(jokes)
+    )
+  }
 
   handleVote(id, delta) {
     this.setState(
